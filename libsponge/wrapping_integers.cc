0b22aea544a6366c966e51563c25d071683f6f5d
llbbllbb2000 <zisheng.cai2000@gmail.com>
2022-10-21 22:32:01 -0700

    lab2-wrap

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -5,8 +5,8 @@
 // For Lab 2, please replace with a real implementation that passes the
 // automated checks run by `make check_lab2`.
 
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
+//template <typename... Targs>
+//void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
@@ -14,8 +14,7 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    return isn + n;
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +28,17 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    uint64_t offset = static_cast<uint32_t>(n - isn) + (static_cast<uint64_t>(checkpoint >> 32) << 32);
+    
+    if (offset < checkpoint) {
+        if (offset + (1ul << 32) - checkpoint < checkpoint - offset) {
+            offset += (1ul << 32);
+        }
+    } else {
+        if (checkpoint >= (1ul << 32) && checkpoint - (offset - (1ul << 32)) < offset - checkpoint) {
+            offset -= (1ul << 32);
+        }
+    }
+
+    return offset;
 }
--- writeups/lab2.md
+++ writeups/lab2.md
@@ -1,21 +1,15 @@
 Lab 2 Writeup
 =============
 
-My name: [your name here]
+My name: llbbllbb2000
 
-My SUNet ID: [your sunetid here]
-
-I collaborated with: [list sunetids here]
-
-I would like to thank/reward these classmates for their help: [list sunetids here]
-
-This lab took me about [n] hours to do. I [did/did not] attend the lab session.
+This lab took me about 10 hours to do. 
 
 Program Structure and Design of the TCPReceiver and wrap/unwrap routines:
 []
 
 Implementation Challenges:
-[]
+1. About unwrap: At first, I counted offset by "(n - isn) + ...". But (n - isn) here is automatically become uint64_t since the offset is uint64_t. So I have to use static_cast<uint32_t>(n - isn) to make sure the result should be a uint32_t number so that it won't be wrapped up to a uint64_t
 
 Remaining Bugs:
 []
